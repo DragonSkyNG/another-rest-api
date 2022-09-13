f0a32427ca07c7605c30b97bfe69f22ee56b82eb
DragonSky_NG <dragonskyng@gmail.com>
2022-09-13 13:13:02 +0300

    Add initial routes for each model

--- routes/birdRoutes.js
+++ routes/birdRoutes.js
@@ -1,4 +1,11 @@
 import express from "express";
 const router = express.Router();
 
+router.post("/create");
+router.get("/get");
+router.get("/get/:id");
+router.post("/update/:id");
+router.post("/delete");
+router.post("/delete/:id");
+
 export default router;
